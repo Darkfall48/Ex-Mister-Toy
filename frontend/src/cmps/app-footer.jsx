e6b7dcfbda4a36b6e54ede486ce255e53c9b6fbf
Darkfall48 <sidney@sebban.be>
2023-01-10 02:18:58 +0200

    Update app-footer.jsx

--- frontend/src/cmps/app-footer.jsx
+++ frontend/src/cmps/app-footer.jsx
@@ -1,9 +1,13 @@
+import { useSelector } from 'react-redux'
 import { UserMsg } from './user-msg.jsx'
 
 export function AppFooter() {
+  const toysCount = useSelector(
+    (storeState) => storeState.toyModule.toys.length
+  )
   return (
     <footer>
-      <h5>Currently X toys in the shop</h5>
+      <h5>Currently {toysCount} toys in the shop</h5>
       <p>Coffeerights to Sidney Sebban</p>
       <UserMsg />
     </footer>
